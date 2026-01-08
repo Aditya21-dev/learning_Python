2598a5bb53b483f754649628dc86ba837db9ab09
Aditya21-dev <adityadas0217@gmail.com>
2026-01-08 22:16:29 +0530

    recursion practice

--- Jan_25/jan_6.py
+++ Jan_25/jan_6.py
@@ -7,9 +7,35 @@
 # print(num_n(10))
 
 
-def num_n(n):
-    if n == 0:
-        return n 
-    return n + num_n(n-1)
+# def num_n(n):
+#     if n == 0:
+#         return n 
+#     return n + num_n(n-1)
 
-print(num_n(50))
+# print(num_n(50))
+
+
+# def num_p(n):
+#     if n == 1:
+#         return 1
+#     return n*num_p(n-1)
+
+# print(num_p(5))
+
+# def revs_S(s):
+#     if s == "":
+#         return s
+#     return revs_S(s[1:]) + s[0]
+
+# print(revs_S("Aditya"))
+
+
+
+
+def fibo(n):
+    if n == 0 or  n == 1:
+        return n
+    # print(n)
+    return fibo(n-1)+ fibo(n-2)
+
+print(fibo(6))
