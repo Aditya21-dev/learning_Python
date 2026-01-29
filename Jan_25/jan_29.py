92af9fa5006ba38c9118bc04f187ff24fdd93f7f
Aditya21-dev <adityadas0217@gmail.com>
2026-01-29 23:48:40 +0530

    dsa questions

--- Jan_25/jan_29.py
+++ Jan_25/jan_29.py
@@ -46,3 +46,16 @@ def binarySearch(nums, target):
             right = mid - 1
 
     return -1
+
+
+def removeDuplicates(nums):
+    if not nums:
+        return 0
+
+    i = 0
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+
+    return i + 1
