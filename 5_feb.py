99de39b399f41ed664d361cc713a005296fab552
Aditya21-dev <adityadas0217@gmail.com>
2026-02-06 10:40:08 +0530

    index , range practice

---
+++ 5_feb.py
@@ -0,0 +1,35 @@
+# fruits = ["apple", "banana", "mango", "grapes"]
+
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[-1])
+# print(fruits[-2])
+
+# # python mein hamesha indexing nikaalne  k liye [] square brackets ka use hot ahai 
+
+# l1 = [1,2,2,4,5]
+
+# print(l1.index(2))
+
+# l2 = [1,4,5]
+
+# print(l2.index())
+
+
+# l3 = [1,2,3,4,5,6,7,8,9,0]
+
+# print(l3.index(2,1,9))
+
+
+my_range = range(1,11) 
+print(list(my_range))
+
+my_range = range(1,11,-1) 
+print(tuple(my_range))
+
+
+my_range = range(-1,-11,-1) 
+print(tuple(my_range))
+
+
+
